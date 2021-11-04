2c4f35dba81cbf255d3d039cab70762cfc4f3c39
DmitriyGj <loger865@yandex.ru>
2021-11-04 14:47:34 +0500

    New

---
+++ js_7/script.js
@@ -0,0 +1,11 @@
+const operations = [];
+const spliter =[".",",","/"];
+let radios = [...document.getElementsByName("day_work")];
+let resultBox= document.getElementsById("result");
+let calcBtn = document.getElementsById("calc_btn");
+let startDate = document.getElementById("start_date");
+let endDate = document.getElementById("end_date");
+
+startDate.addEventListener("keypress",(event)=>{
+
+})
