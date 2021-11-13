c32898bb92fa76401e56c50c2ec31c8ff8c413b9
DmitriyGj <loger865@yandex.ru>
2021-11-13 15:00:21 +0500

    new

--- js_9/script.js
+++ js_9/script.js
@@ -1,32 +1,55 @@
-let bgimgref = new Image();
-let canvases = [...document.getElementsByClassName("auto")]
-let columns = [...document.getElementsByClassName("column")]
-let models = [[[0,0,"Jaguar E-type 1961"],[0,-180,"Rolls-Royce Silver Shadow 1965"],[0,-360,"Astro Martin DB5 1963"]],
+const models = [[[0,0,"Jaguar E-type 1961"],[0,-180,"Rolls-Royce Silver Shadow 1965"],[0,-360,"Astro Martin DB5 1963"]],
              [[-500,0,"MG MGB 1962"],[-500,-180,"Morgan Plus 8 1968"],[-500,-360, "Lotus Esprit 1976"]]]
 let currentModels = [[[0,0 ],[0,1]],[[1,0],[1,1]]]
-let moving = false;
 let stopBtn = document.getElementById("stopBtn")
 stopBtn.disabled = true;
 let startBtn = document.getElementById("startBtn")
 let modelview =document.getElementById("view")
 
 function initalize(){
-    columns.forEach( (column,cindex)=>{
-        let divs = [...column.getElementsByClassName("auto")]
-        divs.forEach((image,imageIndex)=>{
-            image.style.backgroundImage = bgimgref
-            image.style.backgroundPositionY = `${models[cindex][imageIndex][1]}px`
-            image.style.backgroundPositionX = `${models[cindex][imageIndex][0]}px`
-            image.setAttribute("model",models[cindex][imageIndex][3])
+    [...document.getElementsByClassName("column")].forEach( (column,cindex)=>{
+        [...column.getElementsByClassName("auto")].forEach((image,imageIndex)=>{
             image.onmouseover= function (event){
-                modelview.value = event.target.getAttribute("model").toString()
+                modelview.value = models[currentModels[cindex][imageIndex][0]][currentModels[cindex][imageIndex][1]][2]
             };
             image.onmouseout= function (event){
                 modelview.value =""
             }
         })
     })
+    updateImage()
+}
+initalize();
+
+function updateImage(){
+    [...document.getElementsByClassName("column")].forEach((column,cindex)=>{
+        [...column.getElementsByClassName("auto")].forEach((image,imageIndex)=>{
+            image.style.backgroundPositionY = `${models[currentModels[cindex][imageIndex][0]][currentModels[cindex][imageIndex][1]][1]}px`
+            image.style.backgroundPositionX = `${models[currentModels[cindex][imageIndex][0]][currentModels[cindex][imageIndex][1]][0]}px`
+        })
+    })
 }
 
-initalize();
+var timer;
+
+function rotate(){
+    let temp= currentModels[0]
+    currentModels[0] = currentModels[1]
+    currentModels[1] = temp;
+    updateImage();
+}
+
+startBtn.addEventListener("click",(event)=>{
+    event.preventDefault();
+    timer = setInterval(rotate,500)
+    stopBtn.disabled = false;
+    startBtn.disabled = true
+})
+
+stopBtn.addEventListener("click",(event)=>{
+    event.preventDefault();
+    clearInterval(timer)
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+} )
 
