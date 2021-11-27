fbe0fb47374d861adc0652d8932434494c79dced
DmitriyGj <loger865@yandex.ru>
2021-11-27 16:00:04 +0500

    Update script.js

--- js_11/script.js
+++ js_11/script.js
@@ -1,5 +1,5 @@
 function initTextChecker(){
-    let textbox = document.getElementById("answertext")
+    const textbox = document.getElementById("answertext")
     textbox.addEventListener("keypress",event=>{
         if((isNaN(event.key) && event.key !== ".") ||
         ((textbox.value.includes(".") || textbox.value.length === 0 )&& event.key ===".") ){
@@ -13,7 +13,7 @@ initTextChecker()
 document.getElementById("submit_btn").addEventListener("click",event=>{
     if(checkAllFillAnswers()){
         let score = 0.0;
-        let questions = [...document.getElementsByClassName("question")]
+        const questions = [...document.getElementsByClassName("question")]
         if([...questions[0].getElementsByTagName("input")].findIndex(check=>check.checked) === 3) score++
         [...questions[1].getElementsByTagName("input")].filter(item=>item.checked).forEach(elem=>{
             score += parseInt(elem.value)
@@ -30,19 +30,22 @@ document.getElementById("submit_btn").addEventListener("click",event=>{
 })
 
 function checkAllFillAnswers () { 
-    let allInputs = [...document.getElementsByTagName("input")]
-    return allInputs.filter(inp=>inp.getAttribute("type")=== "radio").some(inp=>inp.checked)
-    && allInputs.filter(inp=>inp.getAttribute("type")=== "checkbox").some(inp=>inp.checked)
-    && allInputs.filter(inp => inp.getAttribute("type") ==="text").some(inp=>inp.value)
-    && document.getElementById("answerselect").selectedIndex  !== -1
+    const allInputs = [...document.getElementsByTagName("input")]
+    const radios =  allInputs.filter(inp=>inp.getAttribute("type")=== "radio").some(inp=>inp.checked) 
+    const checkboxes = allInputs.filter(inp=>inp.getAttribute("type")=== "checkbox").some(inp=>inp.checked)
+    const textboxes = allInputs.filter(inp => inp.getAttribute("type") ==="text").some(inp=>inp.value)
+    return radios && checkboxes && textboxes  && document.getElementById("answerselect").selectedIndex  !== -1
         
  };
 
  function showresult(score){
-    if(score < 2)
+    if(score < 2){
         alert("Неудовлетворительно")
-    if(score > 2 && score < 6)
+    }
+    if(score > 2 && score < 6){
         alert("Хорошо")
-    if(score > 5)
+    }
+    if(score > 5){
         alert("Отлично")
+    }
  };
