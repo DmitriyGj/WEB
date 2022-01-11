6afeffc68c309577e054ec3ace8f87d591d2151a
DmitriyGj <loger865@yandex.ru>
2022-01-11 13:14:19 +0500

    comeback

--- udemy js+react/food-dist/js/index.js
+++ udemy js+react/food-dist/js/index.js
@@ -128,9 +128,10 @@ window.addEventListener('DOMContentLoaded',()=>{
         //MenuCards
 
         class MenuCard{
-            constructor(src,alt,title, description, price, parentSelector){
+            constructor(src,alt,title, description, price, parentSelector, ...classes ){
                 this.src = src;
                 this.alt = alt;
+                this.classes = classes;
                 this.title = title;
                 this.description = description;
                 this.price = price;
@@ -145,17 +146,18 @@ window.addEventListener('DOMContentLoaded',()=>{
 
             render(){
                 const element  = document.createElement('div');
-                element.innerHTML = `<div class="menu__item">
-                                        <img src=${this.src} alt= ${this.alt}>
+                if(this.classes.length === 0){
+                    this.classes = ['menu__item']
+                }
+                this.classes.forEach(className => element.classList.add(className));
+                element.innerHTML = `   <img src=${this.src} alt= ${this.alt}>
                                         <h3 class="menu__item-subtitle">${this.title}</h3>
                                         <div class="menu__item-descr">${this.description}</div>
                                         <div class="menu__item-divider"></div>
                                         <div class="menu__item-price">
                                             <div class="menu__item-cost">Цена:</div>
                                             <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                                        </div>
-                                    </div>
-                                    `;
+                                        </div>`;
                 console.log(this.parent);
                 this.parent.append(element);
             }
@@ -168,8 +170,7 @@ window.addEventListener('DOMContentLoaded',()=>{
                     и здоровых людей. Это абсолютно новый продукт 
                     с оптимальной ценой и высоким качеством!`,
                     9
-                    ,".menu .container"
-                    ).render();
+                    ,".menu .container").render();
         new MenuCard("img/tabs/elite.jpg",
                     "elite",
                     `Меню “Премиум”`,
@@ -177,8 +178,7 @@ window.addEventListener('DOMContentLoaded',()=>{
                     дизайн упаковки, но и качественное исполнение блюд. 
                     Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
                     20,
-                    ".menu .container"
-                    ).render();
+                    ".menu .container").render();
         new MenuCard("img/tabs/post.jpg",
                     "post",
                     `Меню "Постное"`,
@@ -187,8 +187,7 @@ window.addEventListener('DOMContentLoaded',()=>{
                     молоко из миндаля, овса, кокоса или гречки, правильное 
                     количество белков за счет тофу и импортных вегетарианских стейков.`,
                     18,
-                    ".menu .container"
-                    ).render();            
+                    ".menu .container").render();            
 });
 
 
